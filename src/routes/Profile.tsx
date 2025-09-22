94f58218748ddbb6c8bb47b4a243ae29813e8998
prashant <prashantkumar855114@gmail.com>
2025-09-22 18:46:51 +0530

    change localhost to prod url

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -53,7 +53,7 @@ const ProfilePage = () => {
 
         try {
             setAccDeleteLoading(true);
-            const res = await fetch("http://localhost:5000/delete-user", {
+            const res = await fetch("https://notesyncc.vercel.app/delete-user", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ userId: user?.id, userEmail: user?.user_metadata?.email }),
